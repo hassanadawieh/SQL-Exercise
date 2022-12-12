19f7ed38bf06c15070f87504f47e433b0668ec9e
Hassan Adawie <adawiehhassan@gmail.com>
2022-12-12 20:10:20 +0200

    Creating Table

--- answers.sh
+++ answers.sh
@@ -5,3 +5,9 @@ SELECT Points FROM students WHERE name='Alex'
 INSERT INTO students(name,age,Gender,Points)VALUES('Hassan','22','M','800');
 UPDATE students set Points=350 WHERE Points=300;
 UPDATE students SET Points=150 WHERE Points=200;
+
+CREATE TABLE graduates (ID INTEGER NULL PRIMARY KEY AUTOINCREMENT,name TEXT NOT NULL UNIQUE,Age INT,Gender TEXT,Points INT,Graduation TEXT);
+INSERT INTO graduates (ID,name,Age,Gender,Points)
+SELECT ID,name,Age,Gender,Points, FROM students WHERE name='layal';
+UPDATE graduates SET Graduation="08/09/2018" WHERE name="layal";
+DELETE FROM students WHERE name="layal";
